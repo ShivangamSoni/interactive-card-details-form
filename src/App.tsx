f8c820bb3109495963f15660be743f4cb2db2a4a
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-11 16:54:12 +0530

    Init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <div className="min-h-screen">
+            <h1 className="bg-emerald-400 text-white text-3xl font-semibold">
+                App
+            </h1>
+        </div>
+    );
+}
