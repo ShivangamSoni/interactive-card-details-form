6ea8df4724c83a16453908bafe5cb5084b5796d6
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-12 18:55:09 +0530

    Add: Completed State

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import Header from "@components/Header";
 import Form from "@components/Form";
+import CompleteState from "@components/CompleteState";
+
+import Button from "@components/Button";
+import { useState } from "react";
 
 export default function App() {
+    const [complete, setComplete] = useState(true);
+
     return (
         <main className="min-h-screen pb-8 font-primary grid grid-rows-[auto_1fr] lg:grid-rows-1 lg:grid-cols-[4fr_6fr] lg:pb-0 xl:grid-cols-[500px_1fr]">
             <Header
@@ -12,62 +18,66 @@ export default function App() {
                 year="32"
             />
 
-            <Form>
-                <Form.Input
-                    label="Cardholder Name"
-                    error=""
-                    inputProps={{
-                        type: "text",
-                        placeholder: "e.g. Jane Appleseed",
-                    }}
-                />
-
-                <Form.Input
-                    label="Card Number"
-                    error=""
-                    inputProps={{
-                        type: "text",
-                        placeholder: "e.g. 1234 5678 9123 0000",
-                    }}
-                />
-
-                <div className="grid grid-cols-2 gap-2">
-                    <div className="grid gap-2">
-                        <Form.Label>Exp. Date (MM/YY)</Form.Label>
-                        <div className="flex gap-2">
-                            <Form.Input
-                                label="Month"
-                                labelVisible={false}
-                                error=""
-                                inputProps={{
-                                    type: "text",
-                                    placeholder: "MM",
-                                }}
-                            />
-                            <Form.Input
-                                label="Year"
-                                labelVisible={false}
-                                error=""
-                                inputProps={{
-                                    type: "text",
-                                    placeholder: "YY",
-                                }}
-                            />
-                        </div>
-                    </div>
-
+            {!complete ? (
+                <Form>
                     <Form.Input
-                        label="CVC"
+                        label="Cardholder Name"
                         error=""
                         inputProps={{
                             type: "text",
-                            placeholder: "e.g. 123",
+                            placeholder: "e.g. Jane Appleseed",
                         }}
                     />
-                </div>
 
-                <Form.Button type="submit">Submit</Form.Button>
-            </Form>
+                    <Form.Input
+                        label="Card Number"
+                        error=""
+                        inputProps={{
+                            type: "text",
+                            placeholder: "e.g. 1234 5678 9123 0000",
+                        }}
+                    />
+
+                    <div className="grid grid-cols-2 gap-2">
+                        <div className="grid gap-2">
+                            <Form.Label>Exp. Date (MM/YY)</Form.Label>
+                            <div className="flex gap-2">
+                                <Form.Input
+                                    label="Month"
+                                    labelVisible={false}
+                                    error=""
+                                    inputProps={{
+                                        type: "text",
+                                        placeholder: "MM",
+                                    }}
+                                />
+                                <Form.Input
+                                    label="Year"
+                                    labelVisible={false}
+                                    error=""
+                                    inputProps={{
+                                        type: "text",
+                                        placeholder: "YY",
+                                    }}
+                                />
+                            </div>
+                        </div>
+
+                        <Form.Input
+                            label="CVC"
+                            error=""
+                            inputProps={{
+                                type: "text",
+                                placeholder: "e.g. 123",
+                            }}
+                        />
+                    </div>
+
+                    <Button type="submit">Submit</Button>
+                </Form>
+            ) : (
+                <CompleteState />
+            )}
         </main>
     );
 }
