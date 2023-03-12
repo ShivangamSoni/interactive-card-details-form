477c53757e1a8223ab344e958853dd6aa454f96d
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-12 14:46:53 +0530

    Add: Assets

---
+++ src/assets/index.ts
@@ -0,0 +1,19 @@
+export { ReactComponent as CardLogo } from "./icons/card-logo.svg";
+export { ReactComponent as CompletedIcon } from "./icons/complete.svg";
+
+import Front from "./bg/card-front.png";
+import Back from "./bg/card-back.png";
+
+import Desktop from "./bg/main-desktop.png";
+import Mobile from "./bg/main-mobile.png";
+
+export const BG = {
+    Card: {
+        Front,
+        Back,
+    },
+    main: {
+        Desktop,
+        Mobile,
+    },
+};
