df23026107a6b51220e85ec15af8df654debbf6e
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-12 14:50:51 +0530

    Add: Alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { resolve } from "path";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgLoader from "vite-plugin-svgr";
@@ -5,4 +7,16 @@ import svgLoader from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), svgLoader()],
+    resolve: {
+        alias: [
+            {
+                find: "@assets",
+                replacement: resolve(__dirname, "src", "assets"),
+            },
+            {
+                find: "@components",
+                replacement: resolve(__dirname, "src", "components"),
+            },
+        ],
+    },
 });
