257e7cc863753f06ed131803a35c52057a143a79
Shivangam Soni <shivangamsoni99@gmail.com>
2023-03-12 14:05:31 +0530

    Update: Custom Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 export default function App() {
     return (
-        <div className="min-h-screen">
-            <h1 className="bg-emerald-400 text-white text-3xl font-semibold">
-                App
+        <div className="min-h-screen font-primary">
+            <h1 className="text-3xl font-semibold p-3">
+                <span className="inline-block p-0.5 bg-gradient-to-r from-violet to-purple rounded-full">
+                    <span className="inline-block px-5 py-2 bg-white rounded-full">
+                        App
+                    </span>
+                </span>
             </h1>
         </div>
     );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                grayishViolet: {
+                    50: "hsl(270, 3%, 87%)",
+                    300: "hsl(279, 6%, 55%)",
+                    700: "hsl(278, 68%, 11%)",
+                },
+                purple: "hsl(278, 94%, 30%)",
+                violet: "hsl(249, 99%, 64%)",
+                error: "hsl(0, 100%, 66%)",
+            },
+            fontFamily: {
+                primary: ["Space Grotesk", "sans-serif"],
+            },
+        },
     },
     plugins: [],
 };
